37b8ce967ca48867f42972b3845f75782c5435df
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-09-27 18:07:15 -0300

    routes config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import Routes from './config/Routes.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <div className="">Hola</div>
-    </div>
+    <BrowserRouter>
+      <Route component={Routes}/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/config/Routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'; // Import Switch from named exports
+
+import Home from '../pages/Home'
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path='/' exact component={Home}/>
+    </Switch>
+  );
+}
+
+export default Routes;
