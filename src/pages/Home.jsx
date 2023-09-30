5082e065cda59e7ee0842d7d553d2731ed494462
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-09-29 21:41:32 -0300

    new styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {;
                 <IseghVideos />
             </div>
             <div className="swiper">
-                <h1>| ISEGH Imagenes</h1>
+                <h1>| ISEGH 2024</h1>
                 <IseghImgs />
             </div>
             <div className="swiper">
