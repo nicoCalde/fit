94335f4f7f6bd58f1301426c26dede00218ec170
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-09-29 22:33:33 -0300

    links grid

--- src/components/swipers/links/Links.jsx
+++ src/components/swipers/links/Links.jsx
@@ -12,18 +12,23 @@ const Links = () => {
     <div className="links-container">
         <a href="https://nicocalde.github.io/mapai" target="_blank" rel="noopener noreferrer">
             <img src={mapa} alt="Mapa Interactivo" />
+            <h2>Mapa Interactivo</h2>
         </a>
         <a href="https://www.ahrcc.org.ar/camara-de-hoteles" target="_blank" rel="noopener noreferrer">
             <img src={hoteles} alt="Cámara de Hoteles" />
+            <h2>Cámara de Hoteles</h2>
         </a>
         <a href="https://www.ahrcc.org.ar/camara-de-cafes" target="_blank" rel="noopener noreferrer">
             <img src={cafes} alt="Cámara de Cafés" />
+            <h2>Cámara de Cafés</h2>
         </a>
         <a href="https://www.ahrcc.org.ar/camara-de-restaurantes" target="_blank" rel="noopener noreferrer">
             <img src={restaurantes} alt="Cámara de Restaurantes" />
+            <h2>Cámara de Restaurantes</h2>
         </a>
         <a href="https://www.ahrcc.org.ar/camara-de-confiterias" target="_blank" rel="noopener noreferrer">
             <img src={confirerias} alt="Cámara de Confiterías" />
+            <h2>Cámara de Confiterias</h2>
         </a>
     </div>
   );
